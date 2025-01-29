edd503a5f7b703233f238eacf182f5913919ee61
Wang Chong <mz1874@aliyun.com>
2025-01-30 06:06:18 +0800

    作用域插槽

---
+++ src-作用于插槽, 被调用的组建决定数据/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <category title="Games">
+      <!-- 具名插槽 -->
+      <template #named-slot>
+        <h3>具名插槽</h3>
+      </template>
+
+      <!-- 作用域插槽（接收子组件传递的数据） -->
+      <template scope="games">
+        <p>作用域插槽：{{ games }}</p>
+      </template>
+    </category>
+  </div>
+</template>
+
+
+
+<script>
+import axios from "axios";
+import category from "@/components/Category/Category.vue";
+
+export default {
+  name: 'App',
+  components: {
+    category,
+  },
+
+  methods: {}
+}
+
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ src-作用于插槽, 被调用的组建决定数据/components/Category/Category.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}</h3>
+    <ul>
+      <li v-for="(item, index) in ListData" :key="index">{{ item }}</li>
+    </ul>
+    <slot name="named-slot"></slot>
+    <!-- 作用域插槽   -->
+    <slot :games="games"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Category',
+  props: ['ListData', 'title'],
+  data() {
+    return {
+      games: ["游戏", "B", "C", "D"],
+      movies: ["电影", "Y", "z", "S"],
+      music: ["音乐", "V"]
+    }
+  },
+};
+</script>
+
+
+<style scoped>
+.category {
+  background-color: #bbb2b2;
+  width: 200px;
+  height: 300px;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
